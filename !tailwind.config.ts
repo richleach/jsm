bf48c850f5a6a2cb625abf723080ba28e03f40c1
Rich Leach <rich@cfsnap.com>
2023-11-28 14:06:00 -0700

    implement base tailwind config and style guide

---
+++ !tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: "#FF70000",
+          100: "#FF1E6",
+        },
+        dark: {
+          100: "#000000",
+          200: "#0F1117",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
